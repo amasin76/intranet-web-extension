eb9e56a35e5dc06c4a3a14a1a145e3f48c287ed1
amasin76 <amasin76@gmail.com>
2023-09-01 23:20:36 +0100

    Add: `counter` to selection filter

--- src/pages/popup/components/Checker/SelectionFilter.tsx
+++ src/pages/popup/components/Checker/SelectionFilter.tsx
@@ -1,5 +1,6 @@
 import { useData } from "../../context/DataContext";
 import data from "./data.json";
+import "./SelectionFilter.scss";
 
 interface RadioProps {
 	className: string;
@@ -10,16 +11,25 @@ interface RadioProps {
 }
 
 const Radio: React.FC<RadioProps> = ({ className, id, name, defaultChecked, label }) => {
-	const { setFilter } = useData();
+	const { setFilter, tasks } = useData();
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setFilter(event.target.value);
 	};
 
+	let count = 0;
+	if (id === "select-greens") {
+		count = tasks.filter((task) => task.status === "yes").length;
+	} else if (id === "select-reds") {
+		count = tasks.filter((task) => task.status === "no").length;
+	}
+
 	return (
 		<label className={className} id={id}>
 			<input type="radio" name={name} defaultChecked={defaultChecked} onChange={handleChange} value={id} />
-			<div>{label}</div>
+			<div className="filter-btn">
+				{label} {count ? <span className="badge">{count}</span> : ""}
+			</div>
 		</label>
 	);
 };
@@ -27,7 +37,6 @@ const Radio: React.FC<RadioProps> = ({ className, id, name, defaultChecked, labe
 export const SelectionFilter: React.FC = () => {
 	return (
 		<div className="selects">
-			<span>Select:</span>
 			{data.map((item) => (
 				<Radio
 					key={item.id}
