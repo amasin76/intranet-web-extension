cdcbf91d7367c638bcd12035e0ca9e5bc1288b52
amasin76 <amasin76@gmail.com>
2023-09-18 03:53:53 +0100

    Update: display only non-zero values of timeUnits

--- src/shared/utils/leftMsToTime.ts
+++ src/shared/utils/leftMsToTime.ts
@@ -1,4 +1,4 @@
-const msToTime = (ms) => {
+const msToTime = (ms: number, showSeconds = true): string => {
 	let seconds = Math.floor(ms / 1000);
 	const days = Math.floor(seconds / 3600 / 24);
 	seconds -= days * 3600 * 24;
@@ -7,12 +7,21 @@ const msToTime = (ms) => {
 	const minutes = Math.floor(seconds / 60);
 	seconds -= minutes * 60;
 
-	const dDisplay = `${days}D`;
-	const hDisplay = `${hours}H`;
-	const mDisplay = `${minutes}M`;
-	const sDisplay = `${seconds}S`;
+	const timeUnits = [
+		{ value: days, unit: "D" },
+		{ value: hours, unit: "H" },
+		{ value: minutes, unit: "M" },
+		{ value: seconds, unit: "S" },
+	];
 
-	return [dDisplay, hDisplay, mDisplay, sDisplay].filter(Boolean).join(" ");
+	if (!showSeconds) {
+		timeUnits.pop();
+	}
+
+	return timeUnits
+		.filter((timeUnit) => timeUnit.value > 0)
+		.map((timeUnit) => `${timeUnit.value}${timeUnit.unit}`)
+		.join(" ");
 };
 
 export default msToTime;
