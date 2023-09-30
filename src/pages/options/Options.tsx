647153080cd9ad9cb9389c403d8aa30c2ce5ff09
amasin76 <amasin76@gmail.com>
2023-09-30 22:05:55 +0100

    Add: options page

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,8 +1,23 @@
-import React from "react";
-import "@pages/options/Options.css";
+import { useState, useEffect } from "react";
+import themeStorage from "@src/shared/storages/themeStorage";
+import UserStyle from "./components/userStyle/";
+import "@assets/style/theme.scss";
 
 const Options: React.FC = () => {
-  return <div className="container text-lime-400">Options</div>;
+	const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+	useEffect(() => {
+		themeStorage.get().then(setTheme);
+		themeStorage.subscribe(() => {
+			themeStorage.get().then(setTheme);
+		});
+	}, []);
+
+	return (
+		<div className="container h-screen mx-auto px-4 sm:px-6 lg:px-60 py-5 bg-gray-900 text-white">
+			<UserStyle />
+		</div>
+	);
 };
 
 export default Options;
--- src/pages/options/index.tsx
+++ src/pages/options/index.tsx
@@ -3,16 +3,19 @@ import { createRoot } from "react-dom/client";
 import Options from "@pages/options/Options";
 import "@pages/options/index.css";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import { attachTwindStyle } from "@src/shared/style/twind";
 
 refreshOnUpdate("pages/options");
 
 function init() {
-  const appContainer = document.querySelector("#app-container");
-  if (!appContainer) {
-    throw new Error("Can not find #app-container");
-  }
-  const root = createRoot(appContainer);
-  root.render(<Options />);
+	const appContainer = document.querySelector("#app-container");
+	if (!appContainer) {
+		throw new Error("Can not find #app-container");
+	}
+	attachTwindStyle(appContainer, document);
+	const root = createRoot(appContainer);
+
+	root.render(<Options />);
 }
 
 init();
