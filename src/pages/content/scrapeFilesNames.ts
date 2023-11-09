18d9d047dbc4f322bc774a33aa7103dbcedba093
amasin76 <amasin76@gmail.com>
2023-11-09 21:05:27 +0100

    Enhance: scraping init files & dirs

--- src/pages/content/scrapeFilesNames.ts
+++ src/pages/content/scrapeFilesNames.ts
@@ -3,34 +3,18 @@ import findElementByText from "./utils/findElementByText";
 function scrapeFileNames(html: string): {
 	dirName: string | undefined;
 	fileNames: string[];
-	isBigProject: boolean;
 } {
 	// Parse the HTML string into a DOM tree
 	const parser = new DOMParser();
 	const doc = parser.parseFromString(html, "text/html");
-
-	// Initialize the variables to store the results
 	const fileNames: string[] = [];
 	const taskCards = doc.querySelectorAll(".task-card");
 	let dirName: string | undefined;
-	let isBigProject = false;
 
 	// Loop through each task card
 	for (let idx = 0; idx < taskCards.length; idx++) {
 		const taskCard = taskCards[idx];
 		const fileElement = findElementByText(taskCard, "li", "File:");
-
-		// If first task card and there is no file name, set isBigProject to true
-		if (idx === 0) {
-			if (!fileElement) {
-				isBigProject = true;
-				break;
-			} else {
-				// Otherwise get dirName
-				const dirElement = findElementByText(taskCard, "li", "Directory:");
-				dirName = dirElement?.querySelector("code")?.textContent;
-			}
-		}
 
 		// If there is no dirElement, set dirName to the repository name
 		if (!dirName) {
@@ -48,31 +32,18 @@ function scrapeFileNames(html: string): {
 		}
 	}
 
-	return { dirName, fileNames, isBigProject };
+	return { dirName, fileNames };
 }
 
-function generateCommandLine(
-	dirName: string | undefined,
-	fileNames: string[],
-	isBigProject: boolean
-): string {
+function generateCommandLine(dirName: string | undefined, fileNames: string[]): string {
 	let commandLine = "";
 	let hasPyFiles = false,
 		hasJsFiles = false,
 		hasExecutableFiles = false;
 	const subDirs = new Set<string>();
 
-	if (!isBigProject) {
-		commandLine = `mkdir "${dirName}" && cd "${dirName}" && touch README.md `;
-	} else {
-		commandLine =
-			"# The current project doesn’t provide a strict file structure (e.g printf, simple shell..). \
-This means that you have the freedom to create your own file structure for this project. \
-Feel free to organize your files in a way that makes sense to you and helps you \
-complete the project successfully🚀.\n\n # If this project is intended to have its own file \
-structure, please report this as a bug on the GitHub issue with project name to fix it";
-		return commandLine;
-	}
+	if (dirName === undefined) return (commandLine = "No files or dirs got found.");
+	commandLine = `mkdir "${dirName}" && cd "${dirName}" && touch README.md `;
 
 	fileNames.forEach((fileName) => {
 		const parts = fileName.split("/");
@@ -100,7 +71,7 @@ structure, please report this as a bug on the GitHub issue with project name to 
 		commandLine += ` && find . -name "*.js" -exec sh -c 'echo "#!/usr/bin/node" > "{}" && chmod u+x "{}"' \\;`;
 	}
 	if (hasExecutableFiles) {
-		commandLine += ` && find . ! -name "*.*" ! -name "README.md" -type f -exec sh -c 'echo "#!/usr/bin/bash" > "{}" && chmod u+x "{}"' \\;`;
+		commandLine += ` && find . ! -name "*.*" ! -name "README.md" -type f -exec sh -c 'echo "#!/usr/bin/env bash" > "{}" && chmod u+x "{}"' \\;`;
 	}
 
 	return commandLine;
@@ -108,8 +79,8 @@ structure, please report this as a bug on the GitHub issue with project name to 
 
 const copyCmdFiles = (sendResponse) => {
 	const html = document.documentElement.outerHTML;
-	const { dirName, fileNames, isBigProject } = scrapeFileNames(html);
-	const commandLine = generateCommandLine(dirName, fileNames, isBigProject);
+	const { dirName, fileNames } = scrapeFileNames(html);
+	const commandLine = generateCommandLine(dirName, fileNames);
 
 	const textarea = document.createElement("textarea");
 	textarea.textContent = commandLine;
